586485f4c4929c2477aa4f9d7f98dd4932dc87c3
Alex Martin <subtleco@Alexs-MBP.lan>
2022-08-06 14:03:44 -0500

    hello github

--- sandbox.sql
+++ sandbox.sql
@@ -1,7 +1,14 @@
 SELECT
-    user.first_name ||' '|| user.last_name as full_name,
-    user.id user_id,
-    game.*
-FROM levelupapi_game game
-JOIN auth_user user
-ON user.id = game.gamer_id
+    u.first_name ||' '|| u.last_name as full_name,
+    e.description,
+    g.title,
+    e.date,
+    e.time,
+    u.id as user_id
+FROM levelupapi_event_attendees a
+JOIN auth_user u
+ON u.id = a.gamer_id
+JOIN levelupapi_event e
+ON a.event_id = e.id
+JOIN levelupapi_game g
+ON e.game_id = g.id
